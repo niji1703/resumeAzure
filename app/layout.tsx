4b5fde743a150f081f4c3fb0aa697fea03be4c15
Kyunghyun Lee <niji1703@gmail.com>
2025-02-11 11:00:01 +0900

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import { Inter, M_PLUS_1p } from "next/font/google";
-import localFont from "next/font/local";
-import "./globals.css";
-import type React from "react"; // Import React
+import { Inter, Noto_Sans_JP } from "next/font/google"
+import localFont from "next/font/local"
+import "./globals.css"
+import type React from "react" // Import React
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
-const mPlus1p = M_PLUS_1p({
+const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
   weight: ["400", "700"],
-  variable: "--font-m-plus-1p",
-});
+  variable: "--font-noto-sans-jp",
+})
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${mPlus1p.variable} font-sans`}>
+    <html lang="en" className={`${notoSansJP.variable} font-sans`}>
       <body>{children}</body>
     </html>
-  );
+  )
 }
